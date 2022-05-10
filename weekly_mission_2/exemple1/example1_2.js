06ac3a767a19dbe2603850a7d8e7c94ec3d31585
Humberto_Ruz <Ruz_s_92@hotmail.com>
2022-05-10 11:39:28 -0500

    Agregando Cambios weekly2

---
+++ weekly_mission_2/exemple1/example1_2.js
@@ -0,0 +1,33 @@
+console.log("primera imprecion -Creacion de un objeto con propiedades")
+
+const Object1={
+
+    name:   "Ruz",
+    age:    29,
+    stack:  ["js","node","react","vue"],
+    Bolog: {
+        url: "github.com/carlogilmar/blog",
+         posts:5
+        }
+}
+console.log("accendiendo al objeto general")
+console.log(Object1)
+
+console.log("accediendo a un objeto especifio")
+console.log(Object1.age)
+//-----------------------------------
+
+
+console.log(ajolonauta)
+
+const ajolonauta={
+    name:"woopa",
+    mission:"Node.js",
+    sayhellowtoExplorers:function(){
+        console.log("Soy el Ajolonauta,Que onda!!")
+    }
+
+}
+
+console.log("Ajolonauta")
+console.log(ajolonauta.sayhellowtoExplorers)
