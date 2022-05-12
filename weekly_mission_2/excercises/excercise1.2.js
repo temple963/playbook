3f0ac70cb928627daba18408b75d153eff245c45
Humberto_Ruz <Ruz_s_92@hotmail.com>
2022-05-12 13:00:31 -0500

    agregando consoles

---
+++ weekly_mission_2/excercises/excercise1.2.js
@@ -0,0 +1,13 @@
+const facebook={
+    name:"Ruz Salvador",
+    age:29,
+    status:"Single",
+    city:"Morelia",
+    work: "Sales Manager",
+
+
+    
+}
+
+console.log(facebook)
+console.log(facebook.name)
