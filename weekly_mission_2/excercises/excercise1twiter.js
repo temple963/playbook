aa88b334f53c76e9410da4da3295ec89d8d861d7
Humberto_Ruz <Ruz_s_92@hotmail.com>
2022-05-12 12:19:40 -0500

    agregando twitter

---
+++ weekly_mission_2/excercises/excercise1twiter.js
@@ -0,0 +1,11 @@
+    const twitter = {
+        name: "Ruz",
+        user:"@Ruz60381464",
+        age: 29,
+        hashtag:"Sexbomb",
+        joined: 2000,
+
+    }
+
+    console.log(twitter.age)
+    console.log(twitter)
